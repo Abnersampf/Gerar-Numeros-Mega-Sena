86ce02bd0df215daaf3b5ad1fade1f2b0c51d488
Abner <58574013+Abnersampf@users.noreply.github.com>
2023-07-25 22:39:25 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 class Program
 {
-    // qtd = quantidade de números aleatórios que você quer gerar;
+    // qtd = quantidade de números aleatórios qur você quer gerar;
     // max = valor máximo que esses números podem ser;
     // min = valor mínimo que esses números podem ser.
     static int qtd = 6, max = 60, min = 1;
@@ -9,28 +9,32 @@ class Program
         int aleatorio;
         int[] numeros = new int[qtd];
         Random random = new Random();
+        bool sair = true;
 
         // Gera os números aleatórios (sem que haja repetição entre eles);
         // OBS: Caso você queira números repetidos, retire o loop while.
-        for (int c = 0; c < numeros.Length; c++)
+        while (sair)
         {
-            aleatorio = random.Next(min, max + 1);
-            while (contem(aleatorio, numeros))
+            for (int c = 0; c < numeros.Length; c++)
             {
                 aleatorio = random.Next(min, max + 1);
+                while (contem(aleatorio, numeros))
+                {
+                    aleatorio = random.Next(min, max + 1);
+                }
+                numeros[c] = aleatorio;
             }
-            numeros[c] = aleatorio;
-        }
 
-        // Ordena os números dentro do vetor em ordem crescente.
-        ordenar(numeros);
+            // Ordena os números dentro do vetor em ordem crescente.
+            ordenar(numeros);
 
-        // Apresenta os números na tela
-        foreach (int n in numeros)
-        {
-            Console.Write(n + " ");
+            // Apresenta os números na tela
+            foreach (int n in numeros)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
         }
-        Console.WriteLine();
     }
     // Função que verifica se o número já existe dentro do vetor.
     static bool contem(int n, int[] vetor)
@@ -47,7 +51,7 @@ class Program
     // Função que ordena os elementos do vetor em ordem crescente.
     static void ordenar(int[] vetor)
     {
-        int menor = max;
+        int menor = max + 1;
         int pos = 0;
         for (int i = 0; i < vetor.Length; i++)
         {
@@ -61,7 +65,7 @@ class Program
             }
             vetor[pos] = vetor[i];
             vetor[i] = menor;
-            menor = max;
+            menor = max + 1;
         }
     }
 }
